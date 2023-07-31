d74af2aa5f2edb2db26df1472b27a64ce1cdd32b
Max Ribeiro <maxwilkson@hotmail.com>
2023-07-31 19:34:46 -0300

    Update home

--- main.go
+++ main.go
@@ -8,10 +8,13 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write(
-			[]byte("<div><h1 style=\"text-align:center\"><strong><span style=\"font-size:24px\">Hello Argo CD&nbsp;</span></strong>🐙</h1></div>
-			<div><p style=\"text-align:center\"><span style=\"font-size:18px\">A GitOps Project&nbsp;</span>😎</p></div>"
-			)
-		)
+			[]byte(`
+			<div style="display:flex; justify-content:center">
+				<h1><strong><span style="font-size:24px">Hello Argo CD&nbsp;</span></strong>🐙</h1>
+			</div>
+			<div style="display:flex; justify-content:center">
+				<p><span style="font-size:18px">A GitOps Project&nbsp;</span>😎</p>
+			</div>`))
 	})
 
 	http.ListenAndServe(":8080", nil)
